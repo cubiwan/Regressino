085aa091859179062c81a3cfc4c172138599cf93
Cubiwan <cubiwan@gmail.com>
2020-03-22 03:14:09 +0100

    Create potential.ino

---
+++ examples/potential/potential.ino
@@ -0,0 +1,52 @@
+#include <PotentialRegression.h>
+
+PotentialRegression pr = PotentialRegression();
+double values[2];
+
+void setup() {
+    Serial.begin(9600);     
+}
+
+void loop() {
+    Serial.println("Start learn");
+    pr.learn(10,1.06);
+    pr.learn(20,1.33);
+    pr.learn(30,1.52);
+    pr.learn(40,1.68);
+    pr.learn(50,1.81);
+    pr.learn(60,1.91);
+    pr.learn(70,2.01);
+    pr.learn(80,2.11);
+
+    Serial.println("End learn");
+
+    for(int i = 0; i < 100; i++){
+        Serial.print("Result (");
+        Serial.print(i);
+        Serial.print("): ");
+        Serial.println(pr.calculate(i));    
+    }
+
+    Serial.print("Correlation: ");
+    Serial.println(pr.correlation());
+
+    Serial.print("Values: ");
+    pr.parameters(values);
+    Serial.print("Y = ");
+    Serial.print(values[0]);
+    Serial.print("*e ^");
+    Serial.print(values[1]); 
+    Serial.println("* X"); 
+
+    Serial.println("Reset ->");
+    pr.reset();
+
+    Serial.print("Values: ");  
+    pr.parameters(values);
+    Serial.print(" a = ");
+    Serial.print(values[0]);
+    Serial.print(" b = ");
+    Serial.println(values[1]); 
+    Serial.println("");
+    delay(5000);
+}
